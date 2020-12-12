c239ad3c0030e51534e775ec5982c4c690b14ee3
QT-HH <hyunheal@naver.com>
2020-12-12 23:56:49 +0900

    aglo

--- BOJ/16455_K번째 수 찾는 함수/16455_K번째 수 찾는 함수.py
+++ BOJ/16455_K번째 수 찾는 함수/16455_K번째 수 찾는 함수.py
@@ -1,2 +1,10 @@
 def kth(a,k):
-    
+    x = [0] * 10001
+    y = [0] * 100000
+    for i in a:
+        sh, re = divmod(i,100000)
+        x[sh] += 1
+        y[sh].append(re)
+
+    cnt = 0
+    for i in x:
