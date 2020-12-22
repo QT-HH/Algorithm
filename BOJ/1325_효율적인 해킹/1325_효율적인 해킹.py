67b941a7f2d8caefba98ab19700a793ce71ccb99
QT-HH <hyunheal@naver.com>
2020-12-22 22:41:23 +0900

    algo

--- BOJ/1325_효율적인 해킹/1325_효율적인 해킹.py
+++ BOJ/1325_효율적인 해킹/1325_효율적인 해킹.py
@@ -1,9 +1,14 @@
 import sys
 sys.stdin = open('input.txt')
 
-
-
 N,M = map(int,sys.stdin.readline().split())
+x = [[0]*(N+1) for _ in range(N+1)]
 for _ in range(M):
     A,B = map(int,sys.stdin.readline().split())
-    
+    x[A][B] = 1
+
+
+
+
+
+
